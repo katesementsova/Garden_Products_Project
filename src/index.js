731016efd1e4996c1f527c638ddda8010cc2b468
Kate.Sementsova <ksem66@gmail.com>
2024-01-03 21:46:30 +0100

    Added Home Page in App.js, Downloaded images, installed fonts, added styles for app.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "./assets/fonts/Montserrat/static/Montserrat-Regular.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<App />);
